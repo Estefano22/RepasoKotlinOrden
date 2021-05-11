341cbd9028149de973868480c00f8a8fa4b6b530
estefano <sandulescustefano8@gmail.com>
2021-05-11 12:46:57 +0200

    hecho1

--- src/main/kotlin/orden.kt
+++ src/main/kotlin/orden.kt
@@ -1,88 +1,56 @@
+import kotlin.random.Random
 
-/*var l1 = mutableListOf("Carlos", "Sergio", "Javier", "Ignacio","Manuel", "Stefanel", "Néstor")
-
-class Humano(var nombre: String, var edad: Int)
-var l2 = mutableListOf(Humano("Carlos", 33), Humano("Sergio", 20), Humano("Javier", 21), Humano("Ignacio", 22),Humano("Manuel", 23), Humano("Stefanel", 24), Humano("Néstor", 25))
-
-class HumanoComparable(var nombre: String, var edad: Int) : Comparable<HumanoComparable> {
-    override fun compareTo(other: HumanoComparable): Int {
-        return edad - other.edad
+class AlumnosComparables(var Nombre : String, var Nota : Int) : Comparable<AlumnosComparables>{
+    override fun compareTo(other: AlumnosComparables): Int {
+        return Nota - other.Nota
     }
-
-class AlumnoComparable(){}
 
     override fun toString(): String {
-        return "$nombre de $edad años"
+        return "$Nombre y ha sacado un $Nota "
     }
 }
+val listadeAlumnos = mutableListOf<AlumnosComparables>()
 
-var l3 = mutableListOf(HumanoComparable("Carlos", 33), HumanoComparable("Sergio", 20), HumanoComparable("Javier", 21), HumanoComparable("Ignacio", 22),HumanoComparable("Manuel", 23), HumanoComparable("Stefanel", 24), HumanoComparable("Néstor", 25))
-
-var vocales = listOf("a", "e", "i", "o", "u")
 fun main() {
 
-    println("Lista original = $l1")
+    var numeros = 0
 
-    // Filtrado
-    val r1 = l1.filter { true }
-    println(r1)
+    val listNumAleatorios = mutableListOf<Int>()
 
-    val r2 = l1.filter {
-        it.startsWith("C")
+    repeat(100){
+        numeros = (0..100).random()
+        listNumAleatorios.add(numeros)
     }
-    println(r2)
+
+    println("----------Ordénalos de menor a mayor----------")
+    println("Numeros ordenados de menor a mayor es: ${listNumAleatorios.sorted()}")
+
+    println("----------Ordénalos de mayor a menor----------")
+    println("Numeros ordenados de mayor a menor es: ${listNumAleatorios.sortedDescending()}")
+
+    val listaNumeMenoraMayor = listNumAleatorios.sorted()
+    val listaNumMayoraMenor = listNumAleatorios.sortedDescending()
+
+    println("Lista nueva de los numeros ordenados de menor a mayor :$listaNumeMenoraMayor")
+    println("Lista nueva de los numeros ordenados de mayor a menor :$listaNumMayoraMenor")
 
 
+    repeat(100){
+    val alumno = AlumnosComparables("Alumno ${it+1}", Nota = Random.nextInt(0,11))
+    listadeAlumnos.add(alumno)
+}
+    println("----------Ordenados por Nota----------")
+    listadeAlumnos.sort()
+    listadeAlumnos.forEach { println(it.toString()) }
 
-    // Ordenación.
 
-    // Sort: Atentos a que aquí no devuelve otra lista sino que ordena la original.
-    // Lista ordenada Alfabéticamente
-    l1.sort()
-    println(l1)
+    println("---------Ordenados por Longitud de nombre----------")
+    listadeAlumnos.sortByDescending { it.Nombre.length }
+    listadeAlumnos.forEach { println(it.toString()) }
 
-    // Sorted: Atento que este si devuelve otra lista.
-    // Lista ordenada Alfabéticamente
-    val o1 = l1.sorted()
-    println(o1)
 
-    // SortBy
-    // Lista ordenada por lengitud del nombre
-    l1.sortByDescending { it.length }
-    println(l1)
-
-    // Lista ordenada por número de vocales
-    l1.sortBy { contarVocales(it) }
-    println(l1)
-
-    // Fíjate que sobre l2 NO deja hacer un sort. Eso es debido a que Humano no es una clase "Comparable"
-    // l2.sort()
-
-    // Fíjate que sobre l3 SI deja hacer un sort.
-    println("Lista original = $l3")
-
-    l3.sort()
-    println(l3)
-
-    println("--------------Ejercicio 1-----------------")
-
+    println("---------Ordenados alfabéticamente----------")
+    listadeAlumnos.sortBy {it.Nombre}
+    listadeAlumnos.forEach { println(it.toString()) }
 
 }
-
-fun contarVocales(s: String):Int {
-    var out = 0
-    s.toLowerCase().forEach {
-        if (vocales.contains(it.toString())) out++
-    }
-    return out
-
-
-
-}
-
-
-
-
-
-
-*/
