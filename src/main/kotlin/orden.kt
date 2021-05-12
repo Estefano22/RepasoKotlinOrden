71ea04965904387e901103a9f50562ae03c345ac
Estefano22 <sandulescustefano8@gmail.com>
2021-05-12 14:24:41 +0200

    hecho

--- src/main/kotlin/orden.kt
+++ src/main/kotlin/orden.kt
@@ -22,35 +22,37 @@ fun main() {
         listNumAleatorios.add(numeros)
     }
 
+    listNumAleatorios.sort()
     println("----------Ordénalos de menor a mayor----------")
-    println("Numeros ordenados de menor a mayor es: ${listNumAleatorios.sorted()}")
+    println("Numeros ordenados de menor a mayor es: $listNumAleatorios")
 
+    listNumAleatorios.sortDescending()
     println("----------Ordénalos de mayor a menor----------")
-    println("Numeros ordenados de mayor a menor es: ${listNumAleatorios.sortedDescending()}")
+    println("Numeros ordenados de mayor a menor es: $listNumAleatorios")
 
-    val listaNumeMenoraMayor = listNumAleatorios.sorted()
-    val listaNumMayoraMenor = listNumAleatorios.sortedDescending()
 
-    println("Lista nueva de los numeros ordenados de menor a mayor :$listaNumeMenoraMayor")
-    println("Lista nueva de los numeros ordenados de mayor a menor :$listaNumMayoraMenor")
+
+    println("Lista nueva de los numeros ordenados de menor a mayor :${listNumAleatorios.sorted()}")
+    println("Lista nueva de los numeros ordenados de mayor a menor :${listNumAleatorios.sortedDescending()}")
 
 
     repeat(100){
-    val alumno = AlumnosComparables("Alumno ${it+1}", Nota = Random.nextInt(0,11))
+    val alumno = AlumnosComparables("Alumno ${it+1}", Nota = Random.nextInt(0,10))
     listadeAlumnos.add(alumno)
 }
     println("----------Ordenados por Nota----------")
-    listadeAlumnos.sort()
+    listadeAlumnos.sortByDescending { it.Nota }
     listadeAlumnos.forEach { println(it.toString()) }
 
 
-    println("---------Ordenados por Longitud de nombre----------")
-    listadeAlumnos.sortByDescending { it.Nombre.length }
+    println("----------Ordenados Alfabeticamnete----------")
+
+    listadeAlumnos.sortBy { it.Nombre }
     listadeAlumnos.forEach { println(it.toString()) }
 
-
-    println("---------Ordenados alfabéticamente----------")
-    listadeAlumnos.sortBy {it.Nombre}
+    println("----------Ordenados longhitud----------")
+    listadeAlumnos.sortBy { it.Nombre.length }
     listadeAlumnos.forEach { println(it.toString()) }
+    
 
 }
